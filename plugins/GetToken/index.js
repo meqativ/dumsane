4389527076281b36381bf180b777f7ce9106cf53
Meqativ <meqativ@gmail.com>
2023-04-05 20:49:48 +0300

    prettify

--- plugins/GetToken/index.js
+++ plugins/GetToken/index.js
@@ -1,42 +1,39 @@
 export default {
-		onLoad: function () {
-			const { metro, commands, logger } = vendetta;
+	onLoad: function () {
+		const { metro, commands, logger } = vendetta;
 
-			const { sendBotMessage } = metro.findByProps("sendBotMessage");
-			const { getToken } = metro.findByProps("getToken");
-			const { sendMessage } = metro.findByProps(
-				"sendMessage",
-				"receiveMessage"
-			);
+		const { sendBotMessage } = metro.findByProps("sendBotMessage");
+		const { getToken } = metro.findByProps("getToken");
+		const { sendMessage } = metro.findByProps("sendMessage", "receiveMessage");
 
-			function exeCute(args, ctx) {
-				const options = new Map(args.map((option) => [option.name, option]));
-				const content = `Token: ${getToken()}`;
-				if (options.get("send").value) {
-					sendMessage(ctx.channel.id, { content });
-				} else {
-					sendBotMessage(ctx.channel.id, content);
-				}
+		function exeCute(args, ctx) {
+			const options = new Map(args.map((option) => [option.name, option]));
+			const content = `Token: ${getToken()}`;
+			if (options.get("send").value) {
+				sendMessage(ctx.channel.id, { content });
+			} else {
+				sendBotMessage(ctx.channel.id, content);
 			}
-			this.onUnload = commands.registerCommand({
-				execute: exeCute,
-				name: "token",
-				displayName: "token",
-				description: "View your token",
-				displayDescription: "View your token",
-				options: [
-					{
-						required: false,
-						type: 5,
-						name: "send",
-						displayName: "send",
-						description: "⛔⛔⛔ Send the token in channel or not?",
-						displayDescription: "⛔⛔⛔ Send the token in the channel or not?",
-					},
-				],
-				applicationId: -1,
-				inputType: 1,
-				type: 1,
-			});
-		},
-	};
+		}
+		this.onUnload = commands.registerCommand({
+			execute: exeCute,
+			name: "token",
+			displayName: "token",
+			description: "View your token",
+			displayDescription: "View your token",
+			options: [
+				{
+					required: false,
+					type: 5,
+					name: "send",
+					displayName: "send",
+					description: "⛔⛔⛔ Send the token in channel or not?",
+					displayDescription: "⛔⛔⛔ Send the token in the channel or not?",
+				},
+			],
+			applicationId: -1,
+			inputType: 1,
+			type: 1,
+		});
+	},
+};
