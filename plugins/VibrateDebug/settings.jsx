dfc7c448a390563377b0218723f4e4b50a3e8c1b
Meqativ <meqativ@gmail.com>
2023-04-19 00:39:25 +0300

    move

--- plugins/VibrateDebug/settings.jsx
+++ plugins/VibrateDebug/settings.jsx
@@ -1,26 +1,38 @@
 const { React, ReactNative } = vendetta.metro.common;
 const {
-  plugin: { storage },
-  storage: { useProxy },
+	plugin: { storage },
+	storage: { useProxy },
 } = vendetta;
 if (!("test" in storage)) storage["test"] = "";
 
+const Button = vendetta.metro.getByProps(
+	"ButtonColors",
+	"ButtonLooks",
+	"ButtonSizes"
+).default;
+
 export default (props) => {
-  useProxy(storage);
-  return (
-    <ReactNative.ScrollView style={{ flex: 1 }}>
-      <ReactNative.Button
-        title="hey"
-        onPress={(h) => console.log("pressed", h)}
-        accessibilityLabel="test button"
-      />
-      <ReactNative.TextInput
-        onChangeText={(v) => (storage["test"] = v)}
-        value={""}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-        multiline={true}
-      />
-    </ReactNative.ScrollView>
-  );
+	useProxy(storage);
+
+	const buttonStyle = { margin: 2 };
+
+	return (
+		<ReactNative.ScrollView style={{ flex: 1 }}>
+			<Button
+				style={buttonStyle}
+				text="hey"
+				color="brand"
+				size="big"
+				disabled={false}
+				onPress={(h) => console.log("pressed", h)}
+			/>
+			<vendetta.components.TextInput
+				onChangeText={(v) => (storage["test"] = v)}
+				value={""}
+				placeholder="useless placeholder"
+				keyboardType="numeric"
+				multiline={true}
+			/>
+		</ReactNative.ScrollView>
+	);
 };
