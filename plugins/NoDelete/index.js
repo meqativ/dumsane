50a6af8d9da67f6640051e2f2d8befd831adcfb3
Meqativ <meqativ@gmail.com>
2023-04-29 14:45:06 +0300

    test

--- plugins/NoDelete/index.js
+++ plugins/NoDelete/index.js
@@ -1,9 +1,14 @@
+
 import settings from "./settings.jsx";
 const {
 	plugin: { storage },
 } = vendetta;
-let deleteable = []; // shitcode (idk how to do otherwise)
-
+let statuses = {};
+/*
+ * message_id: <0|1>
+ * 0 - received
+ * 1 - dismissed
+ */
 const plugin = {
 	settings,
 	onLoad() {
@@ -18,33 +23,35 @@ const plugin = {
 				const [event] = args;
 
 				if (event.type === "MESSAGE_DELETE") {
-					if (deleteable.includes(event.id)) {
-						delete deleteable[deleteable.indexOf(event.id)], args;
-						return args;
-					}
-					deleteable.push(event.id);
-
-					let message = "This message was deleted";
-					if (storage["timestamps"])
-						message += ` (${vendetta.metro.common
-							.moment(new Date())
-							.toLocaleString()})`;
 					if (me || window?.debugpls) console.log("[NoDelete › before]", args);
-					args[0] = {
-						type: "MESSAGE_EDIT_FAILED_AUTOMOD",
-						messageData: {
-							type: 1,
-							message: {
-								channelId: event.channelId,
-								messageId: event.id,
+					if (typeof statuses[event.id] === "undefined") {
+						let message = "This message was deleted";
+						if (storage["timestamps"])
+							message += ` (${vendetta.metro.common
+								.moment(new Date())
+								.toLocaleString()})`;
+
+						args[0] = {
+							type: "MESSAGE_EDIT_FAILED_AUTOMOD",
+							messageData: {
+								type: 1,
+								message: {
+									channelId: event.channelId,
+									messageId: event.id,
+								},
 							},
-						},
-						errorResponseBody: {
-							code: 200000,
-							message,
-						},
-					};
-					if (me || window?.debugpls) console.log("[NoDelete] › after", args);
+							errorResponseBody: {
+								code: 200000,
+								message,
+							},
+						};
+						statuses[event.id] = 0;
+					if (me || window?.debugpls) console.log("[NoDelete › after]", {status: statuses[event.id], args});
+					} else if (statuses[event.id] === 0) {
+						statuses[event.id] = 1
+						delete statuses[event.id];
+					}
+
 					return args;
 				}
 			}
