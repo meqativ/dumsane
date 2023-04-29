6e0ef1f49ce2883d00fe33222e3981d6fbdb102f
Angel <86276165+Angelix1@users.noreply.github.com>
2023-04-29 17:57:13 +0700

    fixed issue with own msg got deleted

--- plugins/NoDelete/index.js
+++ plugins/NoDelete/index.js
@@ -1,51 +1,70 @@
-import settings from "./settings.jsx";
 const {
 	plugin: { storage },
 } = vendetta;
-let deleteable = []; // shitcode (idk how to do otherwise)
+
+let delet = {}; 
+/*
+{
+  <id>: {
+    arg: <[MessageData]>,
+    flag: <1|2>  // 1 = I FUCKING CACHED IT FOR THE FIRST TIME, 2 = DUDE THIS MSG ALREADY CACHED AND DELETED AND MODIFIED
+  }
+}
+
+*/
+
+
+const p = console.log;
 
 const plugin = {
-	settings,
 	onLoad() {
-		const me =
-			vendetta.metro.findByStoreName("UserStore").getCurrentUser().id ===
-			"7442764549462427238";
-
 		this.onUnload = vendetta.patcher.before(
 			"dispatch",
 			vendetta.metro.common.FluxDispatcher,
 			(args) => {
 				const [event] = args;
-
+        let message = "This message was deleted";
 				if (event.type === "MESSAGE_DELETE") {
-					if (deleteable.includes(event.id)) {
-						delete deleteable[deleteable.indexOf(event.id)], args;
+          
+          // if msg hasnt been cached, literally log SHIT
+          if( !delet[event.id] ) {
+            args[0] = {
+  						type: "MESSAGE_EDIT_FAILED_AUTOMOD",
+  						messageData: {
+  							type: 1,
+  							message: {
+  								channelId: event.channelId,
+  								messageId: event.id,
+  							},
+  						},
+  						errorResponseBody: {
+  							code: 200000,
+  							message,
+  						},
+  					};
+            
+            delet[event.id] = {
+              arg: args,
+              flag: 1
+            };
+            
+            return args;
+          }
+          
+          // if msg already cached, check if flag is 1, if so do literally nothing and change flag to 2
+          if( delet[event.id] && delet[event.id]['flag'] == 1 ) {
+            
+            delet[event.id]['flag'] = 2;
+            
+            // console.log(delet)
+            return delet[event.id]["arg"];
+          };
+          
+          // just delet if flag is 2
+          if( delet[event.id] && delet[event.id]['flag'] == 2 ) {
+						delete delet[event.id], delet;
 						return args;
 					}
-					deleteable.push(event.id);
-
-					let message = "This message was deleted";
-					if (storage["timestamps"])
-						message += ` (${vendetta.metro.common
-							.moment(new Date())
-							.toLocaleString()})`;
-					if (me || window?.debugpls) console.log("[NoDelete › before]", args);
-					args[0] = {
-						type: "MESSAGE_EDIT_FAILED_AUTOMOD",
-						messageData: {
-							type: 1,
-							message: {
-								channelId: event.channelId,
-								messageId: event.id,
-							},
-						},
-						errorResponseBody: {
-							code: 200000,
-							message,
-						},
-					};
-					if (me || window?.debugpls) console.log("[NoDelete] › after", args);
-					return args;
 				}
 			}
 		);
