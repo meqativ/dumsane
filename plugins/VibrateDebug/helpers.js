7537f29c942353692d3a380af020c99e503479a4
Meqativ <meqativ@gmail.com>
2023-05-07 17:08:36 +0300

    Fix stupid path and remove log

--- plugins/VibrateDebug/helpers.js
+++ plugins/VibrateDebug/helpers.js
@@ -3,23 +3,6 @@ const {
 	logger,
 } = vendetta;
 
-export function cmd(obj) {
-	if (!("description" in obj) || !("name" in obj))
-		throw new Error("No name or description in the command guh");
-
-	obj.displayName = obj.name;
-	obj.displayDescription = obj.description;
-	if (obj.options) {
-		obj.options = obj.options.map((p) => ({
-			...p,
-			displayName: p.name,
-			displayDescription: p.description,
-		}));
-	}
-	logger.log("[helper › cmd]", obj);
-	return obj;
-}
-
 export function vibrateScheme(scheme) {
 	
 }
