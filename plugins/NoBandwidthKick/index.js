480f66f67b4dae34a654894f938069c1e05f6245
Meqativ <meqativ@gmail.com>
2023-05-08 08:54:11 +0300

    [NoBandwidthKick] Actually commit the code

--- plugins/NoBandwidthKick/index.js
+++ plugins/NoBandwidthKick/index.js
@@ -1,52 +1,14 @@
-
-import settings from "./settings.jsx";
-const {
-	plugin: { storage },
-} = vendetta;
-let deleteable = []; // shitcode (idk how to do otherwise)
-
-const plugin = {
-	settings,
+export default {
 	onLoad() {
-		const me = vendetta.metro.findByStoreName("UserStore").getCurrentUser().id === "744276454946242723";
-
 		this.onUnload = vendetta.patcher.before(
-			"dispatch",
-			vendetta.metro.common.FluxDispatcher,
+			"start",
+			vendetta.metro.findByProps("Timeout").Timeout.prototype,
 			(args) => {
-				const [event] = args;
-
-				if (event.type === "MESSAGE_DELETE") {
-					if (deleteable.includes(event.id)) {
-						delete deleteable[deleteable.indexOf(event.id)], args;
-						return args;
-					}
-					deleteable.push(event.id);
-
-					let message = "This message was deleted";
-					if (storage["timestamps"])
-						message += ` (${vendetta.metro.common.moment().format((storage["ew"])?"hh:mm:ss.SS a":"HH:mm:ss.SS")})`;
-					if (me || window?.debugpls) console.log("[NoDelete › before]", args);
-					args[0] = {
-						type: "MESSAGE_EDIT_FAILED_AUTOMOD",
-						messageData: {
-							type: 1,
-							message: {
-								channelId: event.channelId,
-								messageId: event.id,
-							},
-						},
-						errorResponseBody: {
-							code: 200000,
-							message,
-						},
-					};
-					if (me || window?.debugpls) console.log("[NoDelete] › after", args);
-					return args;
+				if (args[1].name === "disconnect") {
+					args[1] = () => {};
 				}
+				return args;
 			}
 		);
 	},
 };
-
-export default plugin;
