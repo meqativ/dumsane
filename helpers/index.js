3372426651db8c8addbb2bd214a156db5cf1e670
Meqativ <meqativ@gmail.com>
2023-05-27 16:39:53 +0300

    Add translation handling

--- helpers/index.js
+++ helpers/index.js
@@ -1,15 +1,16 @@
-export function cmdDisplays(obj) {
+export function cmdDisplays(obj, translations, locale) {
 	if (!("description" in obj) || !("name" in obj))
 		throw new Error("No name or description in the command guh");
 
-	obj.displayName = obj.name;
-	obj.displayDescription = obj.description;
+	obj.displayName = translations?.names?.[locale] ?? obj.name;
+	obj.displayDescription = translations?.names?.[locale] ?? obj.description;
 	if (obj.options) {
-		obj.options = obj.options.map((p) => ({
-			...p,
-			displayName: p.name,
-			displayDescription: p.description,
-		}));
+		obj.options = obj.options.map((option, optionIndex) => {
+			option.displayName = translations?.options?.[optionIndex]?.names?.[locale] ?? option.name;
+			option.displayDescription = translations?.options?.[optionIndex]?.descriptions?.[locale] ?? option.description;
+			// TODO: handle choices
+			// if (option?.choices)
+		});
 	}
 	return obj;
 }
