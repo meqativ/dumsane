79d83384459cd84e392f473fef61c28f27c9790e
Meqativ <Meqativ@users.noreply.github.com>
2023-05-28 19:08:57 +0000

    deploy: b376d344180507174b692aed469689c2f5a94a1a

--- NoDelete/index.js
+++ NoDelete/index.js
@@ -43,56 +43,66 @@
 	const plugin = {
 	  settings,
 	  onLoad() {
-	    vendetta;
-	    const { FluxDispatcher: FluxDispatcher1 } = vendetta.metro.common;
-	    const getCurrentUser1 = vendetta.metro.findByStoreName("UserStore").getCurrentUser;
-	    this?.onUnload?.();
-	    let currentUser1 = getCurrentUser1();
-	    if (!currentUser1) {
-	      FluxDispatcher1.subscribe("CONNECTION_OPEN", run);
-	    } else {
-	      this.run("meow");
+	    try {
+	      const { plugin: { storage: storage1 } } = vendetta;
+	      const { FluxDispatcher: FluxDispatcher1 } = vendetta.metro.common;
+	      const getCurrentUser1 = vendetta.metro.findByStoreName("UserStore").getCurrentUser;
+	      this?.onUnload?.();
+	      let currentUser1 = getCurrentUser1();
+	      if (!currentUser1) {
+	        FluxDispatcher1.subscribe("CONNECTION_OPEN", run);
+	      } else {
+	        this.run("meow");
+	      }
+	    } catch (e) {
+	      alert(e.stack);
+	      console.error(e);
 	    }
 	  },
 	  run(unsub) {
-	    if (unsub !== "meow")
-	      FluxDispatcher.unsubscribe(run);
-	    if (!currentUser)
-	      currentUser = getCurrentUser();
-	    const me = currentUser.id === "744276454946242723";
-	    this.onUnload = vendetta.patcher.before("dispatch", vendetta.metro.common.FluxDispatcher, function(args) {
-	      const log = window?.debugpls === true || me === true && window?.debugpls !== false;
-	      const [event] = args;
-	      if (event.type === "MESSAGE_DELETE") {
-	        if (deleteable.includes(event.id)) {
-	          delete deleteable[deleteable.indexOf(event.id)];
+	    try {
+	      if (unsub !== "meow")
+	        FluxDispatcher.unsubscribe(run);
+	      if (!currentUser)
+	        currentUser = getCurrentUser();
+	      const me = currentUser.id === "744276454946242723";
+	      this.onUnload = vendetta.patcher.before("dispatch", vendetta.metro.common.FluxDispatcher, function(args) {
+	        const log = window?.debugpls === true || me === true && window?.debugpls !== false;
+	        const [event] = args;
+	        if (event.type === "MESSAGE_DELETE") {
+	          if (deleteable.includes(event.id)) {
+	            delete deleteable[deleteable.indexOf(event.id)], args;
+	            return args;
+	          }
+	          deleteable.push(event.id);
+	          let message = "This message was deleted";
+	          if (storage["timestamps"])
+	            message += ` (${vendetta.metro.common.moment().format(storage["ew"] ? "hh:mm:ss.SS a" : "HH:mm:ss.SS")})`;
+	          if (log)
+	            console.log("[NoDelete \u203A before]", args);
+	          args[0] = {
+	            type: "MESSAGE_EDIT_FAILED_AUTOMOD",
+	            messageData: {
+	              type: 1,
+	              message: {
+	                channelId: event.channelId,
+	                messageId: event.id
+	              }
+	            },
+	            errorResponseBody: {
+	              code: 2e5,
+	              message
+	            }
+	          };
+	          if (log)
+	            console.log("[NoDelete \u203A after]", args);
 	          return args;
 	        }
-	        deleteable.push(event.id);
-	        let message = "This message was deleted";
-	        if (storage["timestamps"])
-	          message += ` (${vendetta.metro.common.moment().format(storage["ew"] ? "hh:mm:ss.SS a" : "HH:mm:ss.SS")})`;
-	        if (log)
-	          console.log("[NoDelete \u203A before]", args);
-	        args[0] = {
-	          type: "MESSAGE_EDIT_FAILED_AUTOMOD",
-	          messageData: {
-	            type: 1,
-	            message: {
-	              channelId: event.channelId,
-	              messageId: event.id
-	            }
-	          },
-	          errorResponseBody: {
-	            code: 2e5,
-	            message
-	          }
-	        };
-	        if (log)
-	          console.log("[NoDelete \u203A after]", args);
-	        return args;
-	      }
-	    });
+	      });
+	    } catch (e) {
+	      alert(e.stack);
+	      console.error(e);
+	    }
 	  }
 	};
 
