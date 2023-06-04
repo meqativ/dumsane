b9f04a500c411a6cebf94fb0cb10c0d5813ecf99
Meqativ <meqativ@gmail.com>
2023-06-04 15:39:37 +0300

    h

--- plugins/Vibrate/commands/start.js
+++ plugins/Vibrate/commands/start.js
@@ -2,183 +2,183 @@ import { generateBasicScheme, parseScheme } from "../schemeUtils.js";
 import * as hlp from "../../../helpers/index.js";
 import { findByStoreName } from "@vendetta/metro";
 import {
-  vibrate,
-  authorMods,
-  PLUGIN_FORUM_POST_URL,
+	vibrate,
+	authorMods,
+	PLUGIN_FORUM_POST_URL,
 	EMBED_COLOR,
-  sendMessage,
+	sendMessage,
 } from "../index.js";
 
 export const command = {
-  exeCute: async (interaction) => {
-    const messageMods = {
-      ...authorMods,
-      interaction: {
-        name: "/vibrate start",
-        user: findByStoreName("UserStore").getCurrentUser(),
-      },
-    };
-    try {
-      const { args, channel } = interaction;
+	exeCute: async (interaction) => {
+		const messageMods = {
+			...authorMods,
+			interaction: {
+				name: "/vibrate start",
+				user: findByStoreName("UserStore").getCurrentUser(),
+			},
+		};
+		try {
+			const { args, channel } = interaction;
 
-      if (
-        !(
-          args.get("scheme") ||
-          args.get("duration") ||
-          args.get("repeat") ||
-          args.get("gap")
-        )
-      ) {
-        return await sendMessage(
-          {
-            channelId: channel.id,
-            embeds: [
-              {
+			if (
+				!(
+					args.get("scheme") ||
+					args.get("duration") ||
+					args.get("repeat") ||
+					args.get("gap")
+				)
+			) {
+				return await sendMessage(
+					{
+						channelId: channel.id,
+						embeds: [
+							{
 								color: EMBED_COLOR(),
-                type: "rich",
-                title: `<${hlp.EMOJIS.getFailure()}> Please provide a \`scheme\` or choose \`duration\`, \`repeat\` and/or \`gap\``,
-              },
-            ],
-          },
-          messageMods
-        );
-      }
-      const generatedScheme =
-        args.get("scheme")?.value ||
-        generateBasicScheme(
-          args.get("duration").value,
-          args.get("repeat")?.value,
-          args.get("gap")?.value
-        );
-      vibrate({
-        scheme: generatedScheme,
-        parseCB: async (vibration) => {
-          return await sendMessage(
-            {
-              channelId: channel.id,
-              embeds: [
-                {
-								color: EMBED_COLOR(),
-                  type: "rich",
-                  title: `<:vibrating:1095354969965731921> Parsing vibration…`,
-                  footer: {
-                    text: `ID: ${vibration.id}\n(if you stop now, it will stop after parsing)`,
-                  },
-                },
-              ],
-            },
-            messageMods
-          );
-        },
+								type: "rich",
+								title: `<${hlp.EMOJIS.getFailure()}> Please provide a \`scheme\` or choose \`duration\`, \`repeat\` and/or \`gap\``,
+							},
+						],
+					},
+					messageMods
+				);
+			}
+			const generatedScheme =
+				args.get("scheme")?.value ||
+				generateBasicScheme(
+					args.get("duration").value,
+					args.get("repeat")?.value,
+					args.get("gap")?.value
+				);
+			vibrate({
+				scheme: generatedScheme,
+				parseCB: async (vibration) => {
+					return await sendMessage(
+						{
+							channelId: channel.id,
+							embeds: [
+								{
+									color: EMBED_COLOR(),
+									type: "rich",
+									title: `<:vibrating:1095354969965731921> Parsing vibration…`,
+									footer: {
+										text: `ID: ${vibration.id}\n(if you stop now, it will stop after parsing)`,
+									},
+								},
+							],
+						},
+						messageMods
+					);
+				},
 				parseFailCB: async (vibration) => {
-          return await sendMessage(
-            {
-              channelId: channel.id,
-              embeds: [
-                {
-								color: EMBED_COLOR(),
-                  type: "rich",
-                  title: `<${hlp.EMOJIS.getFailure()}> An error ocurred while parsing the scheme`,
-                  description: `\`\`\`js\n${vibration.scheme.toString()}\`\`\``,
-                },
-              ],
-            },
-            {
-              ...messageMods,
-              id: vibration.parseCallbackOutput.id,
-              edited_timestamp: Date.now().toString(),
-            }
-          );
-				},	
-        startCB: async (vibration) => {
-          return await sendMessage(
-            {
-              channelId: channel.id,
-              embeds: [
-                {
-								color: EMBED_COLOR(),
-                  type: "rich",
-                  title: `<:vibrating:1095354969965731921> Playing vibration`,
-                  footer: { text: `ID: ${vibration.id}` },
-                },
-              ],
-            },
-            {
-              ...messageMods,
-              edited_timestamp: Date.now().toString(),
-              id: vibration.parseCallbackOutput.id,
-            }
-          );
-        },
-        errorCB: async (vibration) => {
-          return await sendMessage(
-            {
-              channelId: channel.id,
-              embeds: [
-                {
-								color: EMBED_COLOR(),
-                  type: "rich",
-                  title: `${hlp.EMOJIS.getFailure()} An error ocurred while playing the vibration`,
-                  description: `\`\`\`${vibration.error.message}\`\`\``,
-                },
-              ],
-            },
-            {
-              ...messageMods,
-              edited_timestamp: Date.now().toString(),
-              id: vibration.startCallbackOutput.id,
-            }
-          );
-        },
-        finishCB: async (vibration) => {
-          const replyId = vibration.startCallbackOutput.id;
-          sendMessage(
-            {
-              channelId: channel.id,
-              embeds: [
-                {
-								color: EMBED_COLOR(),
-                  type: "rich",
-                  title: `<:still:1095977283212296194> ${
-                    vibration.stopped ? "Stopp" : "Finish"
-                  }ed playing`,
-                  footer: { text: `ID: ${vibration.id}` },
-                },
-              ],
-            },
-            {
-              ...messageMods,
-              type: 19,
-              message_reference: {
-                channel_id: channel.id,
-                message_id: replyId,
-                guild_id: interaction?.guild?.id,
-              },
-              referenced_message: findByStoreName("MessageStore").getMessage(
-                channel.id,
-                replyId
-              ),
-            }
-          );
-        },
-      });
-    } catch (e) {
-      console.error(e);
-      sendMessage(
-        {
-          channelId: channel.id,
-          content: `\`\`\`js\n${e.stack}\`\`\``,
-          embeds: [
-            {
-								color: EMBED_COLOR(),
-              type: "rich",
-              title: `<${hlp.EMOJIS.getFailure()}> An error ocurred while running the command`,
-              description: `Send a screenshot of this error and explain how you came to it, here: ${PLUGIN_FORUM_POST_URL}, to hopefully get this error solved!`,
-            },
-          ],
-        },
-        messageMods
-      );
-    }
-  },
+					return await sendMessage(
+						{
+							channelId: channel.id,
+							embeds: [
+								{
+									color: EMBED_COLOR(),
+									type: "rich",
+									title: `<${hlp.EMOJIS.getFailure()}> An error ocurred while parsing the scheme`,
+									description: `\`\`\`js\n${vibration.scheme.toString()}\`\`\``,
+								},
+							],
+						},
+						{
+							...messageMods,
+							id: vibration.parseCallbackOutput.id,
+							edited_timestamp: Date.now().toString(),
+						}
+					);
+				},
+				startCB: async (vibration) => {
+					return await sendMessage(
+						{
+							channelId: channel.id,
+							embeds: [
+								{
+									color: EMBED_COLOR(),
+									type: "rich",
+									title: `<:vibrating:1095354969965731921> Playing vibration`,
+									footer: { text: `ID: ${vibration.id}` },
+								},
+							],
+						},
+						{
+							...messageMods,
+							edited_timestamp: Date.now().toString(),
+							id: vibration.parseCallbackOutput.id,
+						}
+					);
+				},
+				errorCB: async (vibration) => {
+					return await sendMessage(
+						{
+							channelId: channel.id,
+							embeds: [
+								{
+									color: EMBED_COLOR(),
+									type: "rich",
+									title: `${hlp.EMOJIS.getFailure()} An error ocurred while playing the vibration`,
+									description: `\`\`\`${vibration.error.message}\`\`\``,
+								},
+							],
+						},
+						{
+							...messageMods,
+							edited_timestamp: Date.now().toString(),
+							id: vibration.startCallbackOutput.id,
+						}
+					);
+				},
+				finishCB: async (vibration) => {
+					const replyId = vibration.startCallbackOutput.id;
+					sendMessage(
+						{
+							channelId: channel.id,
+							embeds: [
+								{
+									color: EMBED_COLOR(),
+									type: "rich",
+									title: `<:still:1095977283212296194> ${
+										vibration.stopped ? "Stopp" : "Finish"
+									}ed playing`,
+									footer: { text: `ID: ${vibration.id}` },
+								},
+							],
+						},
+						{
+							...messageMods,
+							type: 19,
+							message_reference: {
+								channel_id: channel.id,
+								message_id: replyId,
+								guild_id: interaction?.guild?.id,
+							},
+							referenced_message: findByStoreName("MessageStore").getMessage(
+								channel.id,
+								replyId
+							),
+						}
+					);
+				},
+			});
+		} catch (e) {
+			console.error(e);
+			sendMessage(
+				{
+					channelId: channel.id,
+					content: `\`\`\`js\n${e.stack}\`\`\``,
+					embeds: [
+						{
+							color: EMBED_COLOR(),
+							type: "rich",
+							title: `<${hlp.EMOJIS.getFailure()}> An error ocurred while running the command`,
+							description: `Send a screenshot of this error and explain how you came to it, here: ${PLUGIN_FORUM_POST_URL}, to hopefully get this error solved!`,
+						},
+					],
+				},
+				messageMods
+			);
+		}
+	},
 };
--- plugins/Vibrate/commands/stop.js
+++ plugins/Vibrate/commands/stop.js
@@ -1,75 +1,75 @@
 import * as hlp from "../../../helpers/index.js";
 import {
 	vibrations,
-  vibrate,
-  authorMods,
-  PLUGIN_FORUM_POST_URL,
-  sendMessage,
-	EMBED_COLOR
+	vibrate,
+	authorMods,
+	PLUGIN_FORUM_POST_URL,
+	sendMessage,
+	EMBED_COLOR,
 } from "../index.js";
-import { findByStoreName, findByProps } from "@vendetta/metro"
+import { findByStoreName, findByProps } from "@vendetta/metro";
 export const command = {
-  exeCute: async (interaction) => {
+	exeCute: async (interaction) => {
 		const { channel, args } = interaction;
-    const messageMods = {
-      ...authorMods,
-      interaction: {
-        name: "/vibrate stop",
-        user: findByStoreName("UserStore").getCurrentUser(),
-      },
-    };
+		const messageMods = {
+			...authorMods,
+			interaction: {
+				name: "/vibrate stop",
+				user: findByStoreName("UserStore").getCurrentUser(),
+			},
+		};
 
-    try {
-      const id = args.get("id").value;
-      if (vibrations.findIndex((v) => v.id === id) === -1) {
-        await sendMessage(
-          {
-            channelId: channel.id,
-            embeds: [
-              {
+		try {
+			const id = args.get("id").value;
+			if (vibrations.findIndex((v) => v.id === id) === -1) {
+				await sendMessage(
+					{
+						channelId: channel.id,
+						embeds: [
+							{
 								color: EMBED_COLOR(),
-                type: "rich",
-                title: `<${hlp.EMOJIS.getFailure()}> Vibration with id \`${id}\` not found`,
-              },
-            ],
-          },
-          messageMods
-        );
-        return;
-      }
-      const vibration = vibrations[vibrations.findIndex((v) => v.id === id)];
-      vibration.stopping = true;
-      vibration.startCallbackOutput = sendMessage(
-        {
-          channelId: channel.id,
-          embeds: [
-            {
-								color: EMBED_COLOR(),
-              type: "rich",
-              title: `<${hlp.EMOJIS.getLoading()}> Stopping vibration…`,
-              footer: { text: `ID: ${vibration.id}` },
-            },
-          ],
-        },
-        messageMods
-      );
-    } catch (e) {
-      console.error(e);
-      sendMessage(
-        {
-								color: EMBED_COLOR(),
-          channelId: channel.id,
-          content: `\`\`\`js\n${e.stack}\`\`\``,
-          embeds: [
-            {
-              type: "rich",
-              title: `<${hlp.EMOJIS.getFailure()}> An error ocurred while running the command`,
-              description: `Send a screenshot of this error and explain how you came to it, here: ${PLUGIN_FORUM_POST_URL}, to hopefully get this error solved!`,
-            },
-          ],
-        },
-        messageMods
-      );
-    }
-  },
+								type: "rich",
+								title: `<${hlp.EMOJIS.getFailure()}> Vibration with id \`${id}\` not found`,
+							},
+						],
+					},
+					messageMods
+				);
+				return;
+			}
+			const vibration = vibrations[vibrations.findIndex((v) => v.id === id)];
+			vibration.stopping = true;
+			vibration.startCallbackOutput = sendMessage(
+				{
+					channelId: channel.id,
+					embeds: [
+						{
+							color: EMBED_COLOR(),
+							type: "rich",
+							title: `<${hlp.EMOJIS.getLoading()}> Stopping vibration…`,
+							footer: { text: `ID: ${vibration.id}` },
+						},
+					],
+				},
+				messageMods
+			);
+		} catch (e) {
+			console.error(e);
+			sendMessage(
+				{
+					color: EMBED_COLOR(),
+					channelId: channel.id,
+					content: `\`\`\`js\n${e.stack}\`\`\``,
+					embeds: [
+						{
+							type: "rich",
+							title: `<${hlp.EMOJIS.getFailure()}> An error ocurred while running the command`,
+							description: `Send a screenshot of this error and explain how you came to it, here: ${PLUGIN_FORUM_POST_URL}, to hopefully get this error solved!`,
+						},
+					],
+				},
+				messageMods
+			);
+		}
+	},
 };
