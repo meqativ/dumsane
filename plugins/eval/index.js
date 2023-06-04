c5219be64d6414b795137e8cff8b261b00791c8b
Meqativ <meqativ@gmail.com>
2023-06-04 17:26:43 +0300

    h

--- plugins/eval/index.js
+++ plugins/eval/index.js
@@ -1,113 +1,179 @@
+import * as hlp from "../../helpers/index.js";
 import { registerCommand } from "@vendetta/commands";
-import { findByProps } from '@vendetta/metro';
+import { findByProps } from "@vendetta/metro";
+import { storage } from "@vendetta/plugin";
+import { semanticColors } from "@vendetta/ui";
+const authorMods = {
+	author: {
+		username: "eval",
+		avatar: "command",
+		avatarURL: hlp.AVATARS.command,
+	},
+};
 
-let unregister;
-async function evaluate(src, isAsync = false) {
-    let result, errored;
-    if (isAsync || src.includes("await")) {
-        if (src.includes(";") && (!src.endsWith(";") || src.includes("\n") || (src.split(';').length) > 2)) {
-            src = `(async function() { ${src} })()`;
-        } else {
-            src = `(async function() { return ${src} })()`;
-        }
-    }
-  
-    let start = new Date().getTime();
-    try {
-        result = eval(src);
-        if (result instanceof Promise) {
-            result = await result;
-        }
-    } catch(e) {
-        result = e;
-        errored = true;
-    }
-  
-    let elapsed = new Date().getTime() - start;
-    return [errored, result, elapsed];
+if (!("stats" in storage)) storage["stats"] = {};
+{
+	const stats = storage["stats"];
+	if (!("runs" in stats))
+		stats.runs = {
+			failed: 0,
+			succeeded: 0,
+		};
+}
+const {
+	meta: { resolveSemanticColor },
+} = findByProps("colors", "meta");
+const ThemeStore = findByStoreName("ThemeStore");
+
+export const EMBED_COLOR = (color) => {
+	parseInt(
+		color === "exploded"
+			? resolveSemanticColor(
+					ThemeStore.theme,
+					semanticColors.BACKGROUND_SECONDARY
+			  ).slice(1)
+			: resolveSemanticColor(
+					ThemeStore.theme,
+					semanticColors.BACKGROUND_SECONDARY
+			  ).slice(1),
+		16
+	);
+};
+/* thanks acquite#0001 (<@581573474296791211>) */
+let madeSendMessage;
+function sendMessage() {
+	if (window.sendMessage) return window.sendMessage?.(...arguments);
+	if (!madeSendMessage) madeSendMessage = hlp.mSendMessage(vendetta);
+	return madeSendMessage(...arguments);
 }
 
-const Clyde = findByProps('createBotMessage');
-const Channels = findByProps('getLastSelectedChannelId');
-const Messages = findByProps("sendMessage");
+async function evaluate(src, ignorePromise, global) {
+	let result, errored;
 
-function sendReply(channelID, content) {
-  const channel = channelID ?? Channels?.getChannelId?.();
-  const msg = Clyde.createBotMessage({ channelId: channel, content: '' });
+	let start = +new Date();
+	try {
+		result = global ? (0, eval)(src) : eval(src);
+		if (result instanceof Promise && !ignorePromise) {
+			result = await result;
+		}
+	} catch (e) {
+		result = e;
+		errored = true;
+	}
 
-  msg.author.username = 'Vendetta';
-
-  if (typeof content === 'string') {
-    msg.content = content;
-  } else {
-    Object.assign(msg, content);
-  }
-
-  Messages.receiveMessage(channel, msg);
+	let elapsed = +new Date() - start;
+	return { errored, result, elapsed };
 }
 
+async function exeCute(interaction) {
+	try {
+		const { channel, args } = interaction;
+		const ignorePromise = [0, 2].includes(args.get("type")?.value);
+		const silent = [1, 2].includes(args.get("type")?.value);
+		const global = !!args.get("global")?.value;
+		const evaluated = await evaluate(src, ignorePromise, global);
+		console.log("[eval › evaluate() result]", evaluated);
+
+		const { errored, result, elapsed } = evaluated;
+
+		if (!silent) {
+			if (errored) {
+				sendMessage({
+					channelId: channel.id,
+					embeds: [
+						{
+							type: "rich",
+							color: EMBED_COLOR("exploded"),
+							description: result.stack.split("\n    at eval (native)")[0],
+						},
+					],
+				});
+			}
+			if (!errored) sendMessage(
+				{
+					channelId: channel.id,
+					content: `\`\`\`js\n${vendetta.metro
+						.findByProps("inspect")
+						.inspect(result)}\`\`\``,
+					embeds: [
+						{
+							type: "rich",
+							color: EMBED_COLOR("satisfactory"),
+							footer: `type: ${typeof result}\ntook: ${elapsed}ms`,
+						},
+					],
+				},
+				messageMods
+			);
+
+		}
+			if (!errored && args.get("return")?.value) return result;
+	} catch (e) {
+		console.error(e);
+		alert("An uncatched error was thrown while running /eval\n" + e.stack);
+	}
+}
 export default {
-  onLoad: () => {
-    unregister = registerCommand({
-        applicationId: "-1",
-
-        name: "eval",
-        displayName: "eval",
-
-        description: "Runs code specified. BE CAREFUL! RUNNING UNVERIFIED CODE HERE CAN LEAD TO YOU BEING TOKEN LOGGED!",
-        displayDescription: "Runs code specified. BE CAREFUL! RUNNING UNVERIFIED CODE HERE CAN LEAD TO YOU BEING TOKEN LOGGED!",
-
-        type: 1, 
-        inputType: 1,
-
-        options: [
-            {
-                name: "type",
-                displayName: "type",
-    
-                description: "The type of the code to run (regular or asynchronous)",
-                displayDescription: "The type of the code to run (regular or asynchronous)",
-    
-                type: 3,
-                required: true,
-
-                choices: [
-                    {
-                        name: "normal",
-                        displayName: "normal",
-                        value: false
-                    },
-                    {
-                        name: "async",
-                        displayName: "async",
-                        value: true
-                    }
-                ]
-            },
-            {
-                name: "source",
-                displayName: "source",
-
-                description: "The source of the code to run",
-                displayDescription: "The source of the code to run",
-
-                type: 3,
-                required: true,
-            },
-        ],
-        execute: async function (args, message) {
-            const type = args.find(o => o.name == "type").value
-            const src = args.find(o => o.name == "source").value
-            const [error, result, elapsed] = await evaluate(src, type);
-            sendReply(message.channel.id, [
-                `${error ? "Failed executing" : "Successfully executed"} in ${elapsed}ms`,
-                `\`\`\`js\n${result}\n\`\`\``
-            ].join('\n'))
-        }
-    });
-  },
-  onUnload: () => {
-    // remove cmd
-    unregister();
-  },
+	meta: vendetta.plugin,
+	onLoad() {
+		this.onUnload = registerCommand(
+			hlp.cmdDisplays({
+				type: 1,
+				inputType: 1,
+				applicationId: "-1",
+				name: "!eval",
+				displayName: "eval",
+				description: "Evaluates code",
+				options: [
+					{
+						required: true,
+						type: 3,
+						name: "code",
+						description: "The code to evaluate",
+						min_length: 1,
+					},
+					{
+						type: 4,
+						name: "type",
+						description: "How to handle the evaluation",
+						choices: [
+							{
+								name: "🟥await returned promise & 🟩show output",
+								value: 0,
+							},
+							{
+								name: "🟩await returned promise & 🟥show output",
+								value: 1,
+							},
+							{
+								name: "🟥await returned promise & 🟥show output",
+								value: 2,
+							},
+							{
+								name: "🟩await returned promise & 🟩show output [default]",
+								value: -1,
+							},
+						],
+					},
+					{
+						type: 5,
+						name: "global",
+						description: "Whether to evaluate in global scope (default: false)",
+					},
+					{
+						type: 5,
+						name: "return",
+						description:
+							"Whether to return the returned value so it works as a real slash command (default: false)",
+					},
+				],
+				execute: async (args, ctx) =>
+					await exeCute({
+						...ctx,
+						args: new Map(args.map((o) => [o.name, o])),
+						plugin: this,
+					}),
+			})
+		);
+	},
 };
