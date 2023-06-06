decc268ac3c01b0af30463b770bf659b23b4ba3f
Meqativ <meqativ@gmail.com>
2023-06-06 22:52:10 +0300

    h

--- plugins/NoDelete/settings.jsx
+++ plugins/NoDelete/settings.jsx
@@ -2,41 +2,68 @@ import { React, ReactNative } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
 import { Forms } from "@vendetta/ui/components";
-if (!storage["ignore"]) storage["ignore"] = {
-	users: [],
-	bots: false
-}
-if (!("timestamps" in storage)) storage["timestamps"] = false;
-if (!("onlyTimestamps" in storage)) storage["onlyTimestamps"] = false;
-if (!("ew" in storage)) storage["ew"] = false; // AM/PM
 
+// make sure settings exist
+storage["ignore"] ??= {
+	users: [],
+	bots: false,
+};
+storage["timestamps"] ??= false;
+storage["onlyTimestamps"] ??= false;
+storage["ew"] ??= false; // AM/PM
 
 export default (props) => {
 	useProxy(storage);
 	return (
 		<ReactNative.ScrollView style={{ flex: 1 }}>
 			{[
-				{ type: "switch", label: "Show the time of deletion", propId: "timestamps" },
-				{ type: "switch", label: "Use AM/PM", propId: "ew" },
+				{
+					type: "switch",
+					label: "Show the time of deletion",
+					setting: storage["timestamps"],
+					onChange(value) {
+						return (this.setting = value);
+					},
+				},
+				{
+					type: "switch",
+					label: "Use AM/PM",
+					setting: storage["ew"],
+					onChange(value) {
+						return (this.setting = value);
+					},
+				},
 				{ type: "separator" },
-				{ type: "switch", label: "Ignore bots" },
-				{ type: "text", label: "The plugin does not keep the messages you've deleted", },
+				{
+					type: "switch",
+					label: "Ignore bots",
+					setting: storage["ignore"]["bots"],
+					onChange(value) {
+						return (this.setting = value);
+					},
+				},
+				{
+					type: "text",
+					label: "The plugin does not keep the messages you've deleted",
+				},
 			].map((config) => {
-				if (config.type === "switch") return (
-					<Forms.FormRow
-						label={config.label}
-						trailing={
-							<Forms.FormSwitch
-								value={storage[config.propId]}
-								onValueChange={(value) => (storage[config.propId] = value)}
-							/>
-						}
-					/>
-				);
-				if (config.type === "text") return (
-					<Forms.FormLabel label={config.label} />
-				);
-				if (config.type === "separator") return (<Forms.FormDivider/>);
+				if (config.type === "switch") {
+					return (
+						<Forms.FormRow
+							label={config.label}
+							trailing={
+								<Forms.FormSwitch
+									value={config.setting}
+									onValueChange={config.onChange}
+								/>
+							}
+						/>
+					);
+				} else if (config.type === "text") {
+					return <Forms.FormLabel label={config.label} />;
+				} else if (config.type === "separator") {
+					return <Forms.FormDivider />;
+				}
 			})}
 		</ReactNative.ScrollView>
 	);
