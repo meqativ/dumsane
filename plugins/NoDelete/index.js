c99d41b60891319d78b1f1526cd4a3e482392f9b
Meqativ <meqativ@gmail.com>
2023-06-06 22:39:23 +0300

    h

--- plugins/NoDelete/index.js
+++ plugins/NoDelete/index.js
@@ -3,64 +3,72 @@ import { FluxDispatcher, moment } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { before as patchBefore } from "@vendetta/patcher";
 import { findByProps, findByStoreName } from "@vendetta/metro";
-let MessageStore;
-//,deleteable = [];
+let MessageStore,
+	deleteable = [];
+
 export default {
 	settings,
+	patches: [
+		/*()=>deleteable=[]*/
+	],
+	onUnload() {
+		this.patches.forEach((up) => up()); // unpatch every patch
+		this.patches = [];
+	},
 	onLoad() {
-		storage["ignore"]["users"] = ["571661221854707713"];
 		try {
-			this.onUnload = patchBefore("dispatch", FluxDispatcher, (args) => {
-				try {
-				if (!MessageStore) MessageStore = findByStoreName("MessageStore");
-				const event = args[0];
-				if (!event) return;
-				if (event?.type === "MESSAGE_DELETE") {
-					if (!("guildId" in event)) return;
+			this.patches.push(
+				patchBefore("dispatch", FluxDispatcher, (args) => {
+					try {
+						if (!MessageStore) MessageStore = findByStoreName("MessageStore");
+						const event = args[0];
 
-					if (!event?.id || !event?.channelId) return;
-					const message = MessageStore.getMessage(event.channelId, event.id);
-					console.log(message, event);
-					if (storage["ignore"]["users"].includes(message?.author?.id)) return;
-					if (storage["ignore"]["bots"] && message?.author?.bot) return;
-					/*if (deleteable.includes(event.id)) {
-						deleteable.splice(deleteable.indexOf(event.id), 1);
-						return args;
-					}
-					deleteable.push(event.id);//*/
-					let redText = storage["onlyTimestamp"]
-						? ""
-						: storage["message"]?.trim?.() || "This message was deleted";
-					if (storage["timestamps"] || storage["onlyTimestamp"])
-						redText += ` (${moment().format(
-							storage["ew"] ? "hh:mm:ss.SS a" : "HH:mm:ss.SS"
-						)})`;
-					console.log("meow", { redText, event });
-					const update = [{
-						type: "MESSAGE_EDIT_FAILED_AUTOMOD",
-						messageData: {
-							type: 1,
-							message: {
-								channelId: event.channelId,
-								messageId: event.id,
+						if (!event || event?.type !== "MESSAGE_DELETE") return;
+						if (!event?.id || !event?.channelId) return;
+
+						const message = MessageStore.getMessage(event.channelId, event.id);
+
+						if (storage["ignore"]["users"].includes(message?.author?.id))
+							return;
+						if (storage["ignore"]["bots"] && message?.author?.bot) return;
+
+						if (deleteable.includes(event.id)) {
+							deleteable.splice(deleteable.indexOf(event.id), 1);
+							return args;
+						}
+						deleteable.push(event.id);
+						let automodMessage =
+							storage["message"] || "This message was deleted";
+						if (storage["timestamps"])
+							automodMessage += ` (${moment().format(
+								storage["ew"] ? "hh:mm:ss.SS a" : "HH:mm:ss.SS"
+							)})`;
+
+						// overwrite the message delete event with the automod edit fail one
+						args[0] = {
+							type: "MESSAGE_EDIT_FAILED_AUTOMOD",
+							messageData: {
+								type: 1,
+								message: {
+									channelId: event.channelId,
+									messageId: event.id,
+								},
 							},
-						},
-						errorResponseBody: {
-							code: 200000,
-							message: redText,
-						},
-					}];
-					console.log(update)
-					return update;
-				}
-				} catch (e) {
-					console.error(e);
-					alert(e.stack)
-				}
-			});
+							errorResponseBody: {
+								code: 200000,
+								message: automodMessage,
+							},
+						};
+						return args;
+					} catch (e) {
+						console.error(e);
+						alert("Nodelete→patch died\n" + e.stack);
+					}
+				})
+			);
 		} catch (e) {
-			alert("NoDelete died\n" + e.stack);
 			console.error(e);
+			alert("NoDelete died\n" + e.stack);
 		}
 	},
 };
