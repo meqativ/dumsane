6a1b3028dbfd75efbb1ec7756e62c7ac0c5b85c5
Meqativ <meqativ@gmail.com>
2023-06-09 01:59:59 +0300

    h

--- plugins/GetToken/index.js
+++ plugins/GetToken/index.js
@@ -5,6 +5,7 @@ import { findByStoreName, findByProps } from "@vendetta/metro";
 import { findInReactTree } from "@vendetta/utils";
 import { setString } from "@vendetta/clipboard";
 import { before as patchBefore } from "@vendetta/patcher"
+import { showToast } from "@vendetta/ui/toasts"
 import { encode as encodeTok, characters2 } from "../../helpers/numberBase64.js";
 const encodeBase64 = findByProps("base64encode").base64encode;
 const {
