d2ded7a1409bacd25920c8bb4c1a8560df25ead3
Meqativ <meqativ@gmail.com>
2023-06-08 19:55:09 +0300

    User context menu button to ignore messages

--- plugins/NoDelete/index.js
+++ plugins/NoDelete/index.js
@@ -3,9 +3,12 @@ import { FluxDispatcher, moment } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { before as patchBefore } from "@vendetta/patcher";
 import { findByProps, findByStoreName } from "@vendetta/metro";
+import { findInReactTree } from "@vendetta/utils";
+import { showToast } from "@vendetta/ui/toasts";
+const variationSelector69 = "󠄴";
+
 let MessageStore,
 	deleteable = [];
-
 export default {
 	settings,
 	patches: [
@@ -38,7 +41,6 @@ export default {
 						}
 						deleteable.push(event.id);
 
-
 						let automodMessage =
 							storage["message"] || "This message was deleted";
 						if (storage["timestamps"])
@@ -64,10 +66,92 @@ export default {
 						return args;
 					} catch (e) {
 						console.error(e);
-						alert("[Nodelete → patch] died\n" + e.stack);
+						alert("[Nodelete → dispatcher patch] died\n" + e.stack);
 					}
 				})
 			);
+
+			/* thanks fres#2400 (<@843448897737064448>) for example patch */
+			const realUnpatch = patchBefore(
+				"render",
+				findByProps("ScrollView").View,
+				(args) => {
+					try {
+						let a = findInReactTree(
+							args,
+							(r) => r.key === ".$UserProfileOverflow"
+						);
+						if (!a || !a.props || a.props.sheetKey !== "UserProfileOverflow")
+							return;
+						const props = a.props.content.props;
+						if (
+							props.options.find((option) =>
+								option?.label?.startsWith?.(variationSelector69)
+							)
+						)
+							return;
+						console.log(a._owner);
+						const focusedUserId = Object.keys(
+							a._owner.stateNode._keyChildMapping
+						)
+							.find(
+								(str) =>
+									a._owner.stateNode._keyChildMapping[str] &&
+									str.match(/(?<=\$UserProfile)\d+/)
+							)
+							?.slice?.(".$UserProfile".length);
+						let position = +props.options[1].isDestructive;
+						if (!storage["ignore"]["users"].includes(focusedUserId)) {
+							props.options.splice(position + 1, 0, {
+								isDestructive: true,
+								label:
+									variationSelector69 + "[NoDelete] Ignore deleted messages",
+								onPress: () => {
+									storage["ignore"]["users"].push(focusedUserId);
+									showToast(
+										`Ignoring deleted messages from ${props.header.title}`
+									);
+									props.hideActionSheet();
+								},
+							});
+						} else {
+							if (props.options[2].isDestructive) position++;
+							props.options.splice(position + 1, 0, {
+								label:
+									variationSelector69 +
+									"[NoDelete] Stop ignoring deleted messages",
+								onPress: () => {
+									storage["ignore"]["users"].splice(
+										storage["ignore"]["users"].findIndex(
+											(userId) => userId === focusedUserId
+										),
+										1
+									);
+									showToast(
+										`Stopped ignoring deleted messages from ${props.header.title}`
+									);
+
+									props.hideActionSheet();
+								},
+							});
+						}
+					} catch (e) {
+						let successful = false;
+						try {
+							successful = realUnpatch();
+						} catch (e) {
+							successful = false;
+						}
+						console.error(e);
+						alert(
+							`[NoDelete → user context menu patch] failed. Patch ${
+								successful ? "dis" : "en"
+							}abled\n` + e.stack
+						);
+					}
+				}
+			);
+			this.patches.push(realUnpatch);
 		} catch (e) {
 			console.error(e);
 			alert("[NoDelete] dead\n" + e.stack);
