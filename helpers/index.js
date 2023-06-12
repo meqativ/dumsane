8244577b844dc4184bc73797d9a589a908d2f799
Meqativ <meqativ@gmail.com>
2023-06-12 15:26:44 +0300

    mmmm

--- helpers/index.js
+++ helpers/index.js
@@ -1,91 +1,65 @@
 export function cmdDisplays(obj, translations, locale) {
-  if (!obj.name || !obj?.description)
-    throw new Error(
-      `No name(${obj?.name}) or description(${obj?.description}) in the passed command (command name: ${obj?.name})`
-    );
+	if (!obj.name || !obj?.description) throw new Error(`No name(${obj?.name}) or description(${obj?.description}) in the passed command (command name: ${obj?.name})`);
 
-  obj.displayName ??= translations?.names?.[locale] ?? obj.name;
-  obj.displayDescription ??= translations?.names?.[locale] ?? obj.description;
-  if (obj.options) {
-    if (!Array.isArray(obj.options))
-      throw new Error(
-        `Options is not an array (received: ${typeof obj.options})`
-      );
-    for (var optionIndex = 0; optionIndex < obj.options.length; optionIndex++) {
-      const option = obj.options[optionIndex];
-      // TODO: Handle subcommands (type 1, 2 probably i forgor)
-      if (!option?.name || !option?.description)
-        throw new Error(
-          `No name(${option?.name}) or description(${option?.description} in the option with index ${optionIndex}`
-        );
-      option.displayName ??=
-        translations?.options?.[optionIndex]?.names?.[locale] ?? option.name;
-      option.displayDescription ??=
-        translations?.options?.[optionIndex]?.descriptions?.[locale] ??
-        option.description;
-      if (option?.choices) {
-        if (!Array.isArray(option?.choices))
-          throw new Error(
-            `Choices is not an array (received: ${typeof option.choices})`
-          );
-        for (
-          var choiceIndex = 0;
-          choiceIndex < option.choices.length;
-          choiceIndex++
-        ) {
-          const choice = option.choices[choiceIndex];
-          if (!choice?.name)
-            throw new Error(
-              `No name of choice with index ${choiceIndex} in option with index ${optionIndex}`
-            );
-          choice.displayName ??=
-            translations?.options?.[optionIndex]?.choices?.[choiceIndex]
-              ?.names?.[locale] ?? choice.name;
-        }
-      }
-    }
-  }
-  return obj;
+	obj.displayName ??= translations?.names?.[locale] ?? obj.name;
+	obj.displayDescription ??= translations?.names?.[locale] ?? obj.description;
+	if (obj.options) {
+		if (!Array.isArray(obj.options)) throw new Error(`Options is not an array (received: ${typeof obj.options})`);
+		for (var optionIndex = 0; optionIndex < obj.options.length; optionIndex++) {
+			const option = obj.options[optionIndex];
+			// TODO: Handle subcommands (type 1, 2 probably i forgor)
+			if (!option?.name || !option?.description) throw new Error(`No name(${option?.name}) or description(${option?.description} in the option with index ${optionIndex}`);
+			option.displayName ??= translations?.options?.[optionIndex]?.names?.[locale] ?? option.name;
+			option.displayDescription ??= translations?.options?.[optionIndex]?.descriptions?.[locale] ?? option.description;
+			if (option?.choices) {
+				if (!Array.isArray(option?.choices)) throw new Error(`Choices is not an array (received: ${typeof option.choices})`);
+				for (var choiceIndex = 0; choiceIndex < option.choices.length; choiceIndex++) {
+					const choice = option.choices[choiceIndex];
+					if (!choice?.name) throw new Error(`No name of choice with index ${choiceIndex} in option with index ${optionIndex}`);
+					choice.displayName ??= translations?.options?.[optionIndex]?.choices?.[choiceIndex]?.names?.[locale] ?? choice.name;
+				}
+			}
+		}
+	}
+	return obj;
 }
 export function generateStr(chars, length = 27) {
-  if (typeof chars !== "string") throw new Error("Passed chars isn't a string");
-  if (chars?.length <= 0) throw new Error("Invalid chars length");
+	if (typeof chars !== "string") throw new Error("Passed chars isn't a string");
+	if (chars?.length <= 0) throw new Error("Invalid chars length");
 
-  let result = "";
+	let result = "";
 
-  for (let i = 0; i < length; i++)
-    result += chars[Math.floor(Math.random() * chars.length)];
+	for (let i = 0; i < length; i++) result += chars[Math.floor(Math.random() * chars.length)];
 
-  return result;
+	return result;
 }
 
 export function mSendMessage(vendetta) {
-  const { metro } = vendetta;
-  const { receiveMessage } = metro.findByProps("sendMessage", "receiveMessage");
-  const { createBotMessage } = metro.findByProps("createBotMessage");
-  const Avatars = metro.findByProps("BOT_AVATARS");
-  return function (message, mod) {
-    message.channelId ??=
-      metro.findByStoreName("SelectedChannelStore").getChannelId() ?? throw new Error("No channel id to receive the message into (channelId)");
-    if (typeof mod !== "undefined" && "author" in mod) {
-      if ("avatar" in mod.author && "avatarURL" in mod.author) {
-        Avatars.BOT_AVATARS[mod.author.avatar] = mod.author.avatarURL;
-        delete mod.author.avatarURL;
-      }
-    }
-    let msg = mod === true ? message : createBotMessage(message);
-    if (typeof mod === "object")
-      msg = vendetta.metro.findByProps("merge").merge(msg, mod);
-    receiveMessage(message.channelId, msg);
-    return msg;
-  };
+	const { metro } = vendetta;
+	const { receiveMessage } = metro.findByProps("sendMessage", "receiveMessage");
+	const { createBotMessage } = metro.findByProps("createBotMessage");
+	const Avatars = metro.findByProps("BOT_AVATARS");
+	return function (message, mod) {
+		message.channelId ??= metro.findByStoreName("SelectedChannelStore").getChannelId();
+		if ([null, undefined].includes(message.channelId)) throw new Error("No channel id to receive the message into (channelId)");
+		if (typeof mod !== "undefined" && "author" in mod) {
+			if ("avatar" in mod.author && "avatarURL" in mod.author) {
+				Avatars.BOT_AVATARS[mod.author.avatar] = mod.author.avatarURL;
+				delete mod.author.avatarURL;
+			}
+		}
+		let msg = mod === true ? message : createBotMessage(message);
+		if (typeof mod === "object") msg = vendetta.metro.findByProps("merge").merge(msg, mod);
+		receiveMessage(message.channelId, msg);
+		return msg;
+	};
 }
 
-export function prettyTypeof(value, raw){
+export function prettyTypeof(value, raw) {
 	const name = [value?.constructor?.name];
 	name[0] ??= "Undefined";
-	if (name[0] !== "Undefined" && (value?.prototype?.constructor === value && typeof value === "function")) {
-		console.log("h")
+	if (name[0] !== "Undefined" && value?.prototype?.constructor === value && typeof value === "function") {
+		console.log("h");
 		name[0] = "Class";
 		name[1] = `(${value.name})`;
 	} else if (value === null) {
@@ -101,38 +75,37 @@ export function prettyTypeof(value, raw){
 	}
 
 	return name.join(" ");
-};
+}
 
 export function makeDefaults(object, defaults) {
 	if (typeof object === "undefined") throw new Error("No object passed to make defaults for");
 	if (typeof defaultd === "undefined") throw new Error("No defaults object passed to make defaults off of");
 
-  for (const [key, value] of Object.entries(object)) {
-    if (typeof defaults[key] === "object") {
-      makeDefaults(defaults[key], object[key]);
-    } else {
-      object[key] ??= defaults[key];
-    }
-  }
+	for (const [key, value] of Object.entries(object)) {
+		if (typeof defaults[key] === "object") {
+			makeDefaults(defaults[key], object[key]);
+		} else {
+			object[key] ??= defaults[key];
+		}
+	}
 }
 
 export const EMOJIS = {
-  loadingDiscordSpinner: "a:loading:1105495814073229393",
-  aol: "a:aol:1108834296359301161",
-  linuth: ":linuth:1110531631409811547",
-  fuckyoy: ":fuckyoy:1108360628302782564",
-  getLoading() {
-    return Math.random() < 0.01 ? this?.aol : this.loadingDiscordSpinner;
-  },
-  getFailure() {
-    return Math.random() < 0.01 ? this?.fuckyoy : this.linuth;
-  },
-  getSuccess() {
-    return "";
-  },
+	loadingDiscordSpinner: "a:loading:1105495814073229393",
+	aol: "a:aol:1108834296359301161",
+	linuth: ":linuth:1110531631409811547",
+	fuckyoy: ":fuckyoy:1108360628302782564",
+	getLoading() {
+		return Math.random() < 0.01 ? this?.aol : this.loadingDiscordSpinner;
+	},
+	getFailure() {
+		return Math.random() < 0.01 ? this?.fuckyoy : this.linuth;
+	},
+	getSuccess() {
+		return "";
+	},
 };
 
 export const AVATARS = {
-  command:
-    "https://cdn.discordapp.com/attachments/1099116247364407337/1112129955053187203/command.png",
+	command: "https://cdn.discordapp.com/attachments/1099116247364407337/1112129955053187203/command.png",
 };
