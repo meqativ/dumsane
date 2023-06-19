c949b2c264d28ba7016ac337061346f8f65384c3
Meqativ <Meqativ@users.noreply.github.com>
2023-06-19 15:18:14 +0000

    deploy: 80eb13edb643d7a56294980d0b1cf323e95d8ac1

---
+++ index.js
@@ -0,0 +1,1 @@
+(function(e){"use strict";var n={onLoad(){alert("Explode"),vendetta.plugins.removePlugin(vendetta.plugin.id)}};return e.default=n,Object.defineProperty(e,"__esModule",{value:!0}),e})({});
