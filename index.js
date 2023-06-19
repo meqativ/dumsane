8161f8abfd8b2edc1c604c072f731f9a949b9dab
Meqativ <Meqativ@users.noreply.github.com>
2023-06-19 15:17:00 +0000

    deploy: 196351f49917bae9a47600e6c16e1eec44cca61e

--- index.js
+++
@@ -1,1 +0,0 @@
-(function(e){"use strict";var n={onLoad(){alert("Explode"),vendetta.plugins.removePlugin(vendetta.plugin.id)}};return e.default=n,Object.defineProperty(e,"__esModule",{value:!0}),e})({});
